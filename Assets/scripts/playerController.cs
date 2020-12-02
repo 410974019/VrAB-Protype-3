c714481779e7eea191125b1546f26dc419f19cdb
tedcgithub <tedcgithub@users.noreply.github.com>
2020-12-02 11:48:24 +0800

    Initial commit

---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    private Rigidbody playerrig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerrig = GetComponent<Rigidbody>();
+        playerrig.AddForce(Vector3.up * 750);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
