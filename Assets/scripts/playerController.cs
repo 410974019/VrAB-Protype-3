f2eebde19b84f6fb3333b8e113f31d523043df95
410974019 <71741596+410974019@users.noreply.github.com>
2020-12-16 11:15:10 +0800

    12/16

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     private Rigidbody playerrig;
+    private Animator playerani;
+    private AudioSource playeraud;
+
+
+    public ParticleSystem smoke; 
+    public ParticleSystem dirt;
+
+    public AudioClip crash;
+    public AudioClip jump;
+
     public float gravityoffset = 10;
     public float speed = 12;
     private bool onGround = true;
@@ -14,6 +24,10 @@ public class playerController : MonoBehaviour
     void Start()
     {
         playerrig = GetComponent<Rigidbody>();
+        playerani = GetComponent<Animator>();
+        playeraud = GetComponent<AudioSource>();
+
+
         Physics.gravity *= gravityoffset;
     }
 
@@ -25,6 +39,9 @@ public class playerController : MonoBehaviour
         {
             playerrig.AddForce(Vector3.up * speed, ForceMode.Impulse);
             onGround = false;
+            playerani.SetTrigger("Jump_trig");
+            dirt.Stop();
+            playeraud.PlayOneShot(jump, 1.0f);
         }
     }
 
@@ -32,13 +49,20 @@ public class playerController : MonoBehaviour
     {
         //onGround = true;
 
-        if (collision.gameObject.CompareTag("ground"))
+        if (collision.gameObject.CompareTag("ground") && gameOver == false)
         {
             onGround = true;
+            dirt.Play();
         }
         else if (collision.gameObject.CompareTag("ob"))
         {
             gameOver = true;
+            playerani.SetBool("Death_b", true);
+            playerani.SetInteger("DeathType_int", 1);
+            smoke.Play();
+            smoke.Play();
+            dirt.Stop();
+            playeraud.PlayOneShot(crash, 1.0f);
             Debug.Log("Game over");
         }
     }
